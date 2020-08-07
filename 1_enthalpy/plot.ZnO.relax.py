f02c0d2b8f6587bd5375aca991a9d83f1cb250a1
Andrej Uhliarik <uhliarik.a@gmail.com>
2020-08-07 18:27:03 +0200

    Change enthalpy plotting script

--- 1_enthalpy/plot.ZnO.relax.py
+++ 1_enthalpy/plot.ZnO.relax.py
@@ -15,29 +15,33 @@ def get_p_enthalpy(mode):
                 try:
                     p = float(directory[12:])
 
-                    for line in open(root+"/"+directory+"/vc-relax.out"):
-                        line = line.strip()
-                        if line.startswith('Final enthalpy'):
-                            # Line has format like: 'Final enthalpy =    -503.2618573805 Ry'
-                            enthalpy = float(line[17:-3])
-
+                    file = open(root+"/"+directory+"/vc-relax.out").read()
+                        if 'Final enthalpy' in file:
+                            start_flag = file.find('Final enthalpy =')
+                            end_flag = file.find('\n', start=start_flag)
+                            enthalpy = float(file[start_flag + len('Final enthalpy ='):end_flag])
+                        else:
+                            # If 'Final enthalpy' is not present, take the last calculated one
+                            start_flag = file.rfind('enthalpy new')
+                            end_flag = file.find('\n', start=start_flag)
+                            enthalpy = float(file[start_flag + len('enthalpy new            ='):end_flag])
                     p_enthalpy.append([p,enthalpy])
                 except:
                     print('error')
     return np.array(sorted(p_enthalpy, key=lambda x: x[0]))
 
 p_enthalpy_RS = get_p_enthalpy('RS')
-p_RS = p_enthalpy_RS[:,0]/10          # GPa
-enthalpy_RS = p_enthalpy_RS[:,1]      # A^3
+p_RS = p_enthalpy_RS[:,0]/10            # GPa
+enthalpy_RS = p_enthalpy_RS[:,1]/2      # A^3, 2 atoms in basis
 
 p_enthalpy_RS = get_p_enthalpy('WU')
 p_WU = p_enthalpy_WU[:,0]/10          # GPa
-enthalpy_WU = p_enthalpy_WU[:,1]      # A^3
+enthalpy_WU = p_enthalpy_WU[:,1]/8    # A^3, 8 atoms in basis
 
 plt.plot(p_RS,enthalpy_RS,'bo',label='RS enthalpy')
 plt.plot(p_WU,enthalpy_WU,'ro',label='WU enthalpy')
 plt.xlabel('Pressure [GPa]')
-plt.ylabel('Enthalpy [Ry]')
+plt.ylabel('Enthalpy/atom [Ry]')
 plt.legend()
 plt.tight_layout()
 plt.savefig("enthalpy.png")
